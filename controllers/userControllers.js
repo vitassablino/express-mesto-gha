71dbdf65044f9327f7d3646e61ef2c7d35f51f94
Viktor <vitassablino1@gmail.com>
2023-07-27 00:57:12 +0300

    upd: login

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -112,6 +112,9 @@ const login = (req, res, next) => {
 
   return User.findUserByCredentials(email, password)
     .then((user) => {
+      if (!user) {
+        res.status(http2.constants.HTTP_STATUS_BAD_REQUEST).send({message: 'Неверный логин или пароль'})
+      }
       const token = jwt.sign(
         { _id: user._id },
         'some-secret-key',
