d8a2a9635f5426c0ac2e379ea24fc872e9786b32
Viktor <vitassablino1@gmail.com>
2023-07-26 23:03:32 +0300

    added npm celebrate

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -1,5 +1,6 @@
 const usersRouter = require('express').Router();
 const {getUsers, getUserById, createUser, updateUser, updateAvatar, getCurrentUser} = require('../controllers/userControllers')
+const { celebrate, Joi } = require('celebrate');
 
 /* Получение всех пользователей */
 usersRouter.get('/users', getUsers);
@@ -11,12 +12,33 @@ usersRouter.get('/users/:id', getUserById);
 //usersRouter.post('/users', createUser);
 
 /* Обновление данных пользователя */
-usersRouter.patch('/users/me', updateUser);
+usersRouter.patch('/users/me',
+celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+  }),
+}),
+updateUser);
 
 /* Обновление аватара пользователя */
-usersRouter.patch('/users/me/avatar', updateAvatar);
+usersRouter.patch('/users/me/avatar',
+celebrate({
+  body: Joi.object().keys({
+    avatar: Joi.string().required()
+      .regex(/^(https?:\/\/)?([\da-z.-]+).([a-z.]{2,6})([/\w.-]*)*\/?$/),
+  }),
+}),
+updateAvatar);
 
 /* получение информации о текущем пользователе */
-usersRouter.get('/users/me', getCurrentUser);
+usersRouter.get('/users/me',
+celebrate({
+  params: Joi.object().keys({
+    id: Joi.string().required().hex().min(24)
+      .max(24),
+  }),
+}),
+getCurrentUser);
 
 module.exports = usersRouter;
