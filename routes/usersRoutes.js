b57f731ce121fdc1f21b2ea8905b4ad0bad9e41a
Viktor <vitassablino1@gmail.com>
2023-07-27 03:03:05 +0300

    Поменяны местами getCurrentUser и getUserById

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -2,6 +2,16 @@ const usersRouter = require('express').Router();
 const {getUsers, getUserById, createUser, updateUser, updateAvatar, getCurrentUser} = require('../controllers/userControllers')
 const { celebrate, Joi } = require('celebrate');
 
+/* получение информации о текущем пользователе */
+usersRouter.get('/users/me',
+/* celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+  }),
+}), */
+getCurrentUser);
+
 /* Получение всех пользователей */
 usersRouter.get('/users', getUsers);
 
@@ -31,14 +41,6 @@ celebrate({
 }),
 updateAvatar);
 
-/* получение информации о текущем пользователе */
-usersRouter.get('/users/me',
-/* celebrate({
-  body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(30),
-  }),
-}), */
-getCurrentUser);
+
 
 module.exports = usersRouter;
